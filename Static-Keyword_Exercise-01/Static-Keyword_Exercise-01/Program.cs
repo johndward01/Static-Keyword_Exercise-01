79965b216d8458b2ac4b7dc20a539909383faed8
johndward01 <johndward01@gmail.com>
2022-09-01 21:43:57 -0500

    init

---
+++ Static-Keyword_Exercise-01/Static-Keyword_Exercise-01/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// TODO: Create a static class called StaticClass
+
+// TODO: Create 3 "regular" classes called RegularClass1, RegularClass2, and RegularClass3
+
+// TODO: Inside of the static class, create a static field called _regularClassesTotal
+
+// TODO: Inside of the constructor of each of the RegularClasses increment the _regularClassesTotal field
+
+// TODO: Instantiate 1 instance of each of the regular classes
+
+// TODO: Print the _regularClassesTotal field to the console (be creative)
